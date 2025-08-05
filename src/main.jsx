5d8b84ac3d9b8990eaa076fa6da6f34beca7654a
Adeel Ahmad <adeelinfiniteloop@gmail.com>
2025-08-05 21:36:49 +0500

    Update main.jsx
    Signed-off-by: Adeel Ahmad <adeelinfiniteloop@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@
   import App from './App.jsx'
 
   createRoot(document.getElementById('root')).render(
-      <BrowserRouter>
-      <App />
-      </BrowserRouter>
+      <StrictMode>
+        <BrowserRouter>
+           <App />
+        </BrowserRouter>
+      </StrictMode>
   )
